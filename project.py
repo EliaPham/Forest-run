a39ed53a62bbe493fc38197591edf95f7c33afa3
Elia Pham <ehtpham123@gmail.com>
2025-05-10 18:38:00 -0500

    creating Girl class

--- project.py
+++ project.py
@@ -25,6 +25,25 @@ Vine = pygame.image.load(os.path.join("Vine.png"))
 Background = pygame.image.load(os.path.join("Setting_background.png"))
 Foreground = pygame.image.load(os.path.join("Setting_foreground.png"))
 
+class Girl:
+    X_Pos = 80
+    Y_Pos = 310
+
+    def __init__(self):
+        self.duck_img = Ducking
+        self.run_img = Running
+        self.Jump_img = Jumping
+
+        self.girl_duck = False
+        self.girl_run = True
+        self.girl_jump = False
+
+        self.step_index = 0
+        self.image = self.run_img[0]
+        self.girl_rect = self.image.get_rect()
+        self.girl_rect.x = self.X_Pos
+        self.girl_rect.y = self.Y_Pos
+
 def main():
     run = True
     clock = pygame.time.Clock()
